18806a6ef7aeff31b3fba69fe654e6873e8c4539
Kirill2005907 <131652233+Kirill2005907@users.noreply.github.com>
2023-04-24 11:02:00 +0300

    dlya Kata Academy

---
+++ Main.java
@@ -0,0 +1,22 @@
+class Cals{
+    public static void main(String[] args)) throws Exception{ args:{}
+        Scanner scanner = new Scanner(System,in);
+    System.out.println('Введи арабские или римские числа')
+            String expression = scanner.nextLine();
+
+    System.out.printLn(parse(expression)));
+
+    }
+
+    public static String parse (String expression) throws Exception {
+    int num1
+    int num2
+    String operator;
+    String result;
+    boolean Kirill;
+    String[] operands = expression.split('[+\\-*/]'); expression "2+3"
+    if(operands.length != 2) throw new Exception;
+    oper = detectpOperation(expression);
+    if (operator == null) throw new Exception
+
+}
